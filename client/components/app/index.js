54888635744aae45ed6930b24d374906969d9383
Suhan Wijaya <suhanw@gmail.com>
2021-03-09 15:55:19 -0500

    rename folders

--- client/components/app/index.js
+++ client/components/app/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import loadable from '@loadable/component';
 import style from './style';
+const Footer = loadable(() => import('components/footer'));
+// const TopNav =  loadable(() => import('partner/components/top-nav'), { ssr: PARTNER_SSR_ENABLED }); // ONLY CSR
+import TopNav from 'partner/components/top-nav'; // SSR works but no initial CSS
 
-const Footer = () => (
-	<div className={style.footer}>Footer</div>
-);
+const App = () => {
+	useEffect(() => {
+		console.log('useEffect')
+	}, [])
+	
+	return (
+		<div className={style.app}>
+			<TopNav />
+			Hello world again
+			<Footer />
+		</div>
+	);
+}
 
-export default Footer;
+export default App; 
--- client/components/footer/index.js
+++ client/components/footer/index.js
@@ -1,22 +1,8 @@
-import React, { useEffect } from 'react';
-import loadable from '@loadable/component';
+import React from 'react';
 import style from './style';
-const Footer = loadable(() => import('components/footer'));
-// const TopNav =  loadable(() => import('partner/components/top-nav'), { ssr: PARTNER_SSR_ENABLED }); // ONLY CSR
-import TopNav from 'partner/components/top-nav'; // SSR works but no initial CSS
 
-const App = () => {
-	useEffect(() => {
-		console.log('useEffect')
-	}, [])
-	
-	return (
-		<div className={style.app}>
-			<TopNav />
-			Hello world again
-			<Footer />
-		</div>
-	);
-}
+const Footer = () => (
+	<div className={style.footer}>Footer</div>
+);
 
-export default App; 
+export default Footer;
