ead695982e2e68f7a63c54a78873547b7686ef9d
Suhan Wijaya <suhanw@gmail.com>
2021-03-10 12:49:20 -0500

    create diff cloudbuild.yml files

--- client/components/app/index.js
+++ client/components/app/index.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import loadable from '@loadable/component';
 import style from './style';
 const Footer = loadable(() => import('components/footer'));
-// const TopNav =  loadable(() => import('partner/components/top-nav'), { ssr: PARTNER_SSR_ENABLED }); // ONLY CSR
-import TopNav from 'partner/components/top-nav'; // SSR works but no initial CSS
+const TopNav =  loadable(() => import('partner/components/top-nav'), { ssr: PARTNER_SSR_ENABLED }); // ONLY CSR
+// import TopNav from 'partner/components/top-nav'; // SSR works but no initial CSS
 
 const App = () => {
 	useEffect(() => {
