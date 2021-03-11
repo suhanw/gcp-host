d284be242ecd04f7eb0120c7be6b895702fdaff6
Suhan Wijaya <suhanw@gmail.com>
2021-03-10 20:57:41 -0500

    avoid pulling in remote loadable stats if boxed

--- webpack.shared.config.js
+++ webpack.shared.config.js
@@ -3,11 +3,18 @@
 const LoadablePlugin = require('@loadable/webpack-plugin')
 const webpack = require('webpack');
 const path = require('path');
-const remoteLoadableStats = require(process.env.NODE_ENV === 'production' ? './remote-build/client/loadable-stats.json' : '../gcp-remote/build/client/loadable-stats.json');
 
-console.log({
-	remoteEntry: remoteLoadableStats.assetsByChunkName.remote[0]
-}); 
+let remoteLoadableStats = {};
+
+if (process.env.PARTNER && process.env.PARTNER !== 'boxed') {
+	remoteLoadableStats = require(process.env.NODE_ENV === 'production' ? './remote-build/client/loadable-stats.json' : '../gcp-remote/build/client/loadable-stats.json');
+}
+
+if (remoteLoadableStats && remoteLoadableStats.assetsByChunkName && remoteLoadableStats.assetsByChunkName.remote && remoteLoadableStats.assetsByChunkName.remote[0]) {
+	console.log({
+		remoteEntry: remoteLoadableStats.assetsByChunkName.remote[0]
+	}); 
+}
 
 module.exports = {
 	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
