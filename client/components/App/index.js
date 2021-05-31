b830ba725aa11cead4a759ef081029821d728840
Suhan Wijaya <suhanw@gmail.com>
2021-05-31 15:40:21 -0400

    updates

--- client/components/App/index.js
+++ client/components/App/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import style from './style';
 
 const App = () => (<>
-	<div className={style.app}>Hello World</div>
+	<div className={style.app}>Hello World!</div>
 	<button onClick={e => alert('Hello You!')}>Say Hello Back!</button>
 </>);
 
--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import express from 'express';
 import App from '../client/components/App';
 
 const app = express();
-const port = 3000;
+const port = process.env.NODE_ENV === 'production' ? 8080 : 3000;
 
 app.use('/build/client', express.static('build/client'));
 
